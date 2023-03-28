52f9672cc77cd278c2c6fce6277a5aba8ea0c70d
Zaylin <chaveszay30@gmail.com>
2023-03-28 17:49:37 -0600

    Modificando las rutas de cita

--- controllers/citas.js
+++ controllers/citas.js
@@ -3,7 +3,7 @@ const Cita = require('../models/citas.js');
 const Usuario = require('../models/users.js');
 const Patient = require('../models/patients.js');
 const Doctor = require('../models/doctor.js');
-const{horarioDisponible}=require('../helpers/horarioDisponible')
+const { horarioDisponible } = require('../helpers/horarioDisponible')
 
 
 
@@ -11,14 +11,14 @@ const citasGET = async (req = request, res = response) => {
     try {
         const citas = await Cita.find({ 'state': true });
         const doctors = await Doctor.find({ 'state': true });
-        /*res.json(
+        res.json(
             {
-                ok:200,
+                ok: 200,
                 doctors,
                 citas
             }
-        );*/
-        res.render("citas", { doctors: doctors, citas: citas });
+        );
+        // res.render("citas", { doctors: doctors, citas: citas });
 
     } catch (error) {
         console.log(err);
@@ -31,8 +31,21 @@ const citasHorarioPOST = async (req = request, res = response) => {
     try {
         const { date, doctor } = req.body;
         console.log(req.body);
-        var ruta = "createCita/" + doctor + "?date=" + date;
-        res.redirect(ruta);
+        // var ruta = "createCita/" + doctor + "?date=" + date;
+        const citas = await Cita.find({
+            $and: [{ 'estado': true }, { 'doctor._id': doctor }, { datetime: { $gte: fecha } },
+            { datetime: { $lt: new Date(fecha.getTime() + 1440 * 60000) } }]
+        }, { datetime: 1, _id: 0 });
+        const hDisponible = horarioDisponible(fecha, citas);
+        res.json(
+            {
+                ok: 200,
+                date,
+                doctor,
+                hDisponible
+            }
+        );
+        // res.redirect(ruta);
     } catch (error) {
         console.log(error);
         throw new Error('Error en el metodo GET');
@@ -52,15 +65,14 @@ const horariosGet = async (req = request, res = response) => {
             { datetime: { $lt: new Date(fecha.getTime() + 1440 * 60000) } }]
         }, { datetime: 1, _id: 0 });
         const hDisponible = horarioDisponible(fecha, citas);
-
         res.json(
             {
-                ok:200,
-                doctors,
-                citas
+                ok: 200,
+                doctor,
+                hDisponible
             }
         );
-        res.render("horariosDisponibles", { horarios: hDisponible, doctor: doctor });
+        // res.render("horariosDisponibles", { horarios: hDisponible, doctor: doctor });
     } catch (error) {
         console.log(error);
         throw new Error('Error en el metodo GET');
@@ -76,19 +88,21 @@ const createCitaGET = async (req = request, res = response) => {
         const doct = await Doctor.find({ '_id': doctor });
         const patient = await Patient.find({});
         const fecha = new Date(date.split('T')[0]);
-        const citas = await Cita.find({"doctor._id": doctor, state: true,
-            datetime: {$gte: fecha.toISOString(),$lt: new Date(fecha.getTime() + 1440 * 60000).toISOString()}});
+        const citas = await Cita.find({
+            "doctor._id": doctor, state: true,
+            datetime: { $gte: fecha.toISOString(), $lt: new Date(fecha.getTime() + 1440 * 60000).toISOString() }
+        });
         const horario = await horarioDisponible(fecha, citas);
-       
-       /* res.json(
+
+        res.json(
             {
-                ok:200,
-                 patients: patient,
-                 doct: doct[0], 
-                 horario: horario
+                ok: 200,
+                patients: patient,
+                doct: doct[0],
+                horario: horario
             }
-        );*/
-       res.render("createCita", {  patients: patient,doct: doct[0], horario: horario });
+        );
+        //res.render("createCita", {  patients: patient,doct: doct[0], horario: horario });
     } catch (error) {
         console.log(error);
     }
@@ -105,23 +119,31 @@ const saveCitaPOST = async (req = request, res = response) => {
         datetime = new Date(horario);
         const cita = new Cita({ patient, datetime, doctor });
         await cita.save();
-        res.redirect('/api/citas');
+        res.json({
+            ok:200,
+            cita
+        })
+        // res.redirect('/api/citas');
     } catch (error) {
         console.log(error);
     }
 }
 
-const deteleCita=async(req=request, res=response)=>{
-
+const deteleCita = async (req = request, res = response) => {
     try {
-       const {id}=req.params;
-       const cita= await Cita.findByIdAndUpdate(id,{'estado':false});
-       res.redirect('/api/citas');
+        const { id } = req.params;
+        const cita = await Cita.findByIdAndUpdate(id, { 'estado': false });
+        res.json({
+            ok: 200,
+            msg: "Mensaje desde el metodo Delete",
+            cita,
+          });
+        //  res.redirect('/api/citas');
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         throw new Error('Error en el metodo DELETE');
-}
+    }
 }
 
 
@@ -129,7 +151,7 @@ const deteleCita=async(req=request, res=response)=>{
 module.exports = {
     citasGET,
     citasHorarioPOST,
-   horariosGet,
+    horariosGet,
     createCitaGET,
     saveCitaPOST,
     deteleCita
