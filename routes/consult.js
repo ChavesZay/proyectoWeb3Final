093dd06f7ee2f2ef115068fe6b12795ba2ec1281
Zaylin <chaveszay30@gmail.com>
2023-04-27 16:01:29 -0600

    Agregando validaciones

--- routes/consult.js
+++ routes/consult.js
@@ -54,6 +54,7 @@ router.post("/", [
       ),
     check('height', 'La altura del paciente es obligatorio').not().isEmpty(),
     check('pressure', 'La presión del paciente es obligatorio').not().isEmpty(),
+    check('pressure', 'La presión del paciente es en formato de número').isInt(),
     check('symptoms', 'Los sintomas del paciente es obligatorio').not().isEmpty(),
     validate_fields, validarPaciente,], consultPOST);
 
